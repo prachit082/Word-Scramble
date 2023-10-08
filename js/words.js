956d26d8c79e5be87a02519afe96626e2fd3d331
Prachit Pandit <92202695+prachit082@users.noreply.github.com>
2023-10-08 12:23:02 +0530

    Added New Words -  words.js

--- js/words.js
+++ js/words.js
@@ -1,94 +1,142 @@
 let words = [
-    {
-        word: "addition",
-        hint: "The process of adding numbers"
-    },
-    {
-        word: "meeting",
-        hint: "Event in which people come together"
-    },
-    {
-        word: "number",
-        hint: "Math symbol used for counting"
-    },
-    {
-        word: "exchange",
-        hint: "The act of trading"
-    },
-    {
-        word: "canvas",
-        hint: "Piece of fabric for oil painting"
-    },
-    {
-        word: "garden",
-        hint: "Space for planting flower and plant"
-    },
-    {
-        word: "position",
-        hint: "Location of someone or something"
-    },
-    {
-        word: "feather",
-        hint: "Hair like outer covering of bird"
-    },
-    {
-        word: "comfort",
-        hint: "A pleasant feeling of relaxation"
-    },
-    {
-        word: "tongue",
-        hint: "The muscular organ of mouth"
-    },
-    {
-        word: "expansion",
-        hint: "The process of increase or grow"
-    },
-    {
-        word: "country",
-        hint: "A politically identified region"
-    },
-    {
-        word: "group",
-        hint: "A number of objects or persons"
-    },
-    {
-        word: "taste",
-        hint: "Ability of tongue to detect flavour"
-    },
-    {
-        word: "store",
-        hint: "Large shop where goods are traded"
-    },
-    {
-        word: "field",
-        hint: "Area of land for farming activities"
-    },
-    {
-        word: "friend",
-        hint: "Person other than a family member"
-    },
-    {
-        word: "pocket",
-        hint: "A bag for carrying small items"
-    },
-    {
-        word: "needle",
-        hint: "A thin and sharp metal pin"
-    },
-    {
-        word: "expert",
-        hint: "Person with extensive knowledge"
-    },
-    {
-        word: "statement",
-        hint: "A declaration of something"
-    },
-    {
-        word: "second",
-        hint: "One-sixtieth of a minute"
-    },
-    {
-        word: "library",
-        hint: "Place containing collection of books"
-    },
-]
+  {
+    word: "addition",
+    hint: "The process of adding numbers",
+  },
+  {
+    word: "meeting",
+    hint: "Event in which people come together",
+  },
+  {
+    word: "number",
+    hint: "Math symbol used for counting",
+  },
+  {
+    word: "exchange",
+    hint: "The act of trading",
+  },
+  {
+    word: "canvas",
+    hint: "Piece of fabric for oil painting",
+  },
+  {
+    word: "garden",
+    hint: "Space for planting flower and plant",
+  },
+  {
+    word: "position",
+    hint: "Location of someone or something",
+  },
+  {
+    word: "feather",
+    hint: "Hair like outer covering of bird",
+  },
+  {
+    word: "comfort",
+    hint: "A pleasant feeling of relaxation",
+  },
+  {
+    word: "tongue",
+    hint: "The muscular organ of mouth",
+  },
+  {
+    word: "expansion",
+    hint: "The process of increase or grow",
+  },
+  {
+    word: "country",
+    hint: "A politically identified region",
+  },
+  {
+    word: "group",
+    hint: "A number of objects or persons",
+  },
+  {
+    word: "taste",
+    hint: "Ability of tongue to detect flavour",
+  },
+  {
+    word: "store",
+    hint: "Large shop where goods are traded",
+  },
+  {
+    word: "field",
+    hint: "Area of land for farming activities",
+  },
+  {
+    word: "friend",
+    hint: "Person other than a family member",
+  },
+  {
+    word: "pocket",
+    hint: "A bag for carrying small items",
+  },
+  {
+    word: "needle",
+    hint: "A thin and sharp metal pin",
+  },
+  {
+    word: "expert",
+    hint: "Person with extensive knowledge",
+  },
+  {
+    word: "statement",
+    hint: "A declaration of something",
+  },
+  {
+    word: "second",
+    hint: "One-sixtieth of a minute",
+  },
+  {
+    word: "library",
+    hint: "Place containing collection of books",
+  },
+  {
+    word: "polyglot",
+    hint: "One who knows many languages",
+  },
+  {
+    word: "autopsy",
+    hint: "An examination of dead bodies",
+  },
+  {
+    word: "cosmopolitan",
+    hint: "A person who regards the whole world as his country",
+  },
+  {
+    word: "immigrant",
+    hint: " A person coming to a foreign land to settle",
+  },
+  {
+    word: "colleague",
+    hint: "An associate in a office or institution",
+  },
+  {
+    word: "pseudonym",
+    hint: "A name assumed by a writer or a false name",
+  },
+  {
+    word: "atheist",
+    hint: "A person who does not believe in God",
+  },
+  {
+    word: "dwarf",
+    hint: "A person, animal or plant much below the usual height",
+  },
+  {
+    word: "bohemian",
+    hint: "One who does not follow the usual rules of social life ",
+  },
+  {
+    word: "rhetoric",
+    hint: "The art of effective or persuasive speaking or writing",
+  },
+  {
+    word: "tungsten",
+    hint: "Element that is most often used for light bulb filaments",
+  },
+  {
+    word: "violet",
+    hint: "Colour that is at the bottom edge of a rainbow",
+  },
+];
